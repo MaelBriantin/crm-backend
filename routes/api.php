b8dfc91dda86d7d3c527e76916e24103667e406a
Maël Briantin <mael.briantin@proton.me>
2024-02-29 00:03:26 +0100

    feat(brands): update HTTP methods for sector, postcode, and brand routes

--- routes/api.php
+++ routes/api.php
@@ -26,21 +26,21 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/sectorsWithPostcodes/{sector}', [SectorController::class, 'showWithPostcodes']);
     Route::get('/sectorsWithPostcodes', [SectorController::class, 'indexWithPostcodes']);
     Route::delete('/sectors/{sector}', [SectorController::class, 'destroy']);
-    Route::put('/sectors/{sector}', [SectorController::class, 'update']);
+    Route::patch('/sectors/{sector}', [SectorController::class, 'update']);
     Route::get('/sectors/{sector}', [SectorController::class, 'show']);
     Route::post('/sectors', [SectorController::class, 'store']);
     Route::get('/sectors', [SectorController::class, 'index']);
 
     // Posctodes routes
     Route::delete('/postcodes/{postcode}', [PostcodeController::class, 'destroy']);
-    Route::put('/postcodes/{postcode}', [PostcodeController::class, 'update']);
+    Route::patch('/postcodes/{postcode}', [PostcodeController::class, 'update']);
     Route::get('/postcodes/{postcode}', [PostcodeController::class, 'show']);
     Route::post('/postcodes', [PostcodeController::class, 'store']);
     Route::get('/postcodes', [PostcodeController::class, 'index']);
 
     // Brands routes
     Route::delete('/brands/{brand}', [BrandController::class, 'destroy']);
-    Route::put('/brands/{brand}', [BrandController::class, 'update']);
+    Route::patch('/brands/{brand}', [BrandController::class, 'update']);
     Route::get('/brands/{brand}', [BrandController::class, 'show']);
     Route::post('/brands', [BrandController::class, 'store']);
     Route::get('/brands', [BrandController::class, 'index']);
