71cb2b95fb633ed41180148665f22380714f04ee
Maël Briantin <mael.briantin@proton.me>
2024-03-03 22:56:17 +0100

    feat(customers): update French translations for customers and postcodes

---
+++ resources/lang/fr/customers.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    "invalid_sector_id" => "Il semble y avoir un problème avec le secteur entré pour ce client. Veuillez vérifier que ce secteur existe et réessayer.",
+    "creation_error" => "Une erreur est survenue lors de l'enregistrement de ce client. Veuillez vérifier les informations entrée et réessayer.",
+];
