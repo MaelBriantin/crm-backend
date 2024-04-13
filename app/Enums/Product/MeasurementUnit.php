d9b6a0102a774c12b978d948685c9214a90d8229
Maël Briantin <mael.briantin@proton.me>
2024-04-13 13:40:10 +0200

    feat(products): refactor MeasurementUnit enum to include a new constant and additional methods

--- app/Enums/Product/MeasurementUnit.php
+++ app/Enums/Product/MeasurementUnit.php
@@ -4,12 +4,28 @@ namespace App\Enums\Product;
 
 enum MeasurementUnit
 {
+    const NONE = null;
     const MILLIGRAM = 'mg';
     const GRAM = 'g';
     const KILOGRAM = 'kg';
     const MILLILITER = 'ml';
     const CENTILITER = 'cl';
     const LITER = 'l';
+    const DECILITER = 'dl';
+
+    public static function toLabelValue(): array
+    {
+        return [
+            // self::NONE => trans('measurement_units.'.self::NONE),
+            trans('products.measurement_units.'.self::MILLIGRAM) => self::MILLIGRAM,
+            trans('products.measurement_units.'.self::GRAM) => self::GRAM,
+            trans('products.measurement_units.'.self::KILOGRAM) => self::KILOGRAM,
+            trans('products.measurement_units.'.self::MILLILITER) => self::MILLILITER,
+            trans('products.measurement_units.'.self::CENTILITER) => self::CENTILITER,
+            trans('products.measurement_units.'.self::LITER) => self::LITER,
+            trans('products.measurement_units.'.self::DECILITER) => self::DECILITER
+        ];
+    }
 
     public static function toArray(): array
     {
@@ -20,6 +36,12 @@ enum MeasurementUnit
             self::MILLILITER,
             self::CENTILITER,
             self::LITER,
+            self::DECILITER
         ];
+    }
+
+    public static function request(): string
+    {
+        return implode(',', self::toArray());
     }
 }
