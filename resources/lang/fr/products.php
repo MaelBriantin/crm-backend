d595827d48b462178bb409ed33d459362c959b47
Maël Briantin <mael.briantin@proton.me>
2024-04-13 13:41:35 +0200

    feat(products): refactor translation strings for visit frequencies and product-related enums in French language files

---
+++ resources/lang/fr/products.php
@@ -0,0 +1,23 @@
+<?php
+    return [
+        'product_types' => [
+            'default' => 'Hygiène et entretien',
+            'clothes' => 'Vêtements',
+        ],
+        'vat_rates' => [
+            'none' => 'Aucun',
+            '5_5' => '5,5 %',
+            '10' => '10 %',
+            '20' => '20 %',
+        ],
+        'measurement_units' => [
+            null => 'Aucun',
+            'ml' => 'Millilitre (ml)',
+            'l' => 'Litre (L)',
+            'cl' => 'Centilitre (cl)',
+            'dl' => 'Décilitre (dl)',
+            'mg' => 'Milligramme (mg)',
+            'g' => 'Gramme (g)',
+            'kg' => 'Kilogramme (kg)',
+        ],
+    ];
