aefa994af5045bb2cc43cff287a797ae5caad696
MaelBriantin <mael.briantin@proton.me>
2024-06-19 15:18:40 +0200

    feat(customer-stats): add intercom_number column declaration in users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,6 +17,7 @@ return new class extends Migration
             $table->string('company_name')->nullable();
             $table->string('siret')->nullable();
             $table->string('siren')->nullable();
+            $table->string('intercom_number')->nullable();
             $table->string('address')->nullable();
             $table->integer('postcode')->nullable();
             $table->string('city')->nullable();
